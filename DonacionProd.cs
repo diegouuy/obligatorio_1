1f38f417f4b426e46abdbf195c1b9243e20bc38a
edhys <49659746+edhyssantos@users.noreply.github.com>
2020-04-22 23:46:18 -0300

    precarga
    precarga

--- DonacionProd.cs
+++ DonacionProd.cs
@@ -30,7 +30,7 @@ namespace Obligatorio1
         #endregion
 
         #region Metodos
-        public DonacionProd(float valorDonacion, float valorDesc, DateTime fecha, Centro centroRecepcion): base(fecha,centroRecepcion)
+        public DonacionProd(decimal valorDonacion, decimal valorDesc, DateTime fecha, Centro centroRecepcion): base(fecha,centroRecepcion)
         {
             this.valorDonacion = valorDonacion;
             this.valorDesc = valorDesc;
